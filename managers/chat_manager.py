a45ec01d261cd2f38c7c94a85ae9106727540dc1
Indranil Banerjee <bindranil880@gmail.com>
2024-05-05 02:31:18 +0530

    AUTOUI-3: Unnecessary embed agent removed

--- managers/chat_manager.py
+++ managers/chat_manager.py
@@ -1,5 +1,4 @@
 from agents import (
-    embedding_agent as embedder,
     code_writer_agent as coder,
     user_proxy_agent as user,
     code_reviewer_agent as reviewer,
@@ -9,7 +8,6 @@ from impl.chat_manager_builder import ChatManagerBuilder
 chat_manager = ChatManagerBuilder(
     [
         user.user_proxy_agent,
-        embedder.embedding_agent,
         coder.code_writer_agent,
         reviewer.code_reviewer_agent,
     ],
